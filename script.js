cd72f327b11900a5b759f396f2bab2f907b01143
grantpf <90522418+grantpf@users.noreply.github.com>
2022-07-21 21:22:56 -0400

    Add functions to get choice from computer, play 5 rounds and return results.

---
+++ script.js
@@ -0,0 +1,40 @@
+//randomly return rock, paper, or scissors
+function getComputerChoice() {
+    let items = Array("rock", "paper", "scissors");
+    return items[Math.floor(Math.random()*items.length)];
+};
+
+//play a round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {return "tied"}
+    else if (computerSelection === "paper" && playerSelection === "rock" || computerSelection === "scissors" && playerSelection === "paper" || computerSelection === "rock" && playerSelection === "scissors") {
+        return 'lose';
+    }
+
+    else {
+        return 'win';
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    // your code here!
+    const playerSelection = prompt("rock, paper, scissors, shoot!");
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    function declare() {
+        if (result === 'tie') {
+            return("It's a tie!");
+        }
+        else if (result === 'win') {
+            return(`you win! ${playerSelection} beats ${computerSelection}`);
+            
+        }
+        else {
+            return(`you lose! ${computerSelection} beats ${playerSelection}`);
+        }
+    }
+console.log(declare());
+}
+//get player selection
+
+//return string that declares winner
