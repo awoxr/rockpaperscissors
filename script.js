54690f1a4627d6d12c7e84566ee047e90dca2b74
grantpf <90522418+grantpf@users.noreply.github.com>
2022-08-14 00:46:38 -0400

    Add UI with with styling, best out of 5 rounds

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const buttons = document.querySelectorAll('button');
+const playerChoice = document.getElementById('playerChoice');
+const computerChoice = document.getElementById('computerChoice');
+const winner = document.getElementById('winner');
+const playerDiv = document.getElementById('playerscore');
+const computerDiv = document.getElementById('computerscore');
+const playerPoints = document.getElementById('playerscore');
+const computerPoints = document.getElementById('computerscore');
+const counter = document.getElementById('counter');
+
 //randomly return rock, paper, or scissors
 function getComputerChoice() {
     let items = Array("rock", "paper", "scissors");
@@ -7,6 +17,7 @@ function getComputerChoice() {
 //play a round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {return "tie"}
+
     else if (computerSelection === "paper" && playerSelection === "rock" || computerSelection === "scissors" && playerSelection === "paper" || computerSelection === "rock" && playerSelection === "scissors") {
         return 'lose';
     }
@@ -20,36 +31,91 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const buttons = document.querySelectorAll('button');
+//declare final score
+function finalScore() {
+    if (playerScore > computerScore) {
+        return `You win! The score was ${playerScore} to ${computerScore}.`;
+    }
+    else if (playerScore < computerScore) {
+        return `You lose! The score was ${playerScore} to ${computerScore}`;
+    }
+    else {
+        return "You and the computer tied!";
+    }
+    }
 
+
+
+let roundCounter = 0;
+
+//adds a point after the round
+let playerScore = 0;
+let computerScore = 0;
+function score(result) {
+    if (result === 'win') {
+        playerScore += 1;
+        winner.style.backgroundColor ='green';
+    }
+    else if (result === 'lose') {
+        winner.style.backgroundColor ='red';
+        computerScore += 1;
+    }
+    else {
+        winner.style.backgroundColor ='purple';
+        return; 
+    }
+}
+
+//check if it is the final round and declare winner if so
+function roundCheck(round) {
+    if (round === 5) {
+        alert(finalScore());
+        playerScore = 0;
+        computerScore = 0;
+        roundCounter = 0;
+        counter.textContent = `Round ${roundCounter+1}`;
+        playerPoints.textContent = `You: ${playerScore}`;
+        computerPoints.textContent = `Computer: ${computerScore}`;
+        playerChoice.innerHTML = `Your Choice:`;
+        computerChoice.innerHTML = `Computer's Choice:`;
+        winner.innerHTML = `Result:`;
+        winner.style.backgroundColor = 'lightgrey';
+    }
+    else {
+        return;
+    }
+}
+
+//play round on click
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(e) {
         const playerSelection = (this.id);
         const computerSelection = getComputerChoice();
-        console.log(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-});
-}
+        playerChoice.innerHTML = `Your Choice: <b>${playerSelection}</b>.`
+        computerChoice.innerHTML = `Computer's Choice: <b>${computerSelection}</b>.`
+        let result = playRound(playerSelection, computerSelection);
+        winner.innerHTML = `Result: <b>${result}</b>`;
+        score(result);
+        playerPoints.textContent = `You: ${playerScore}`;
+        computerPoints.textContent = `Computer: ${computerScore}`;
+        roundCounter +=1;
+        roundCheck(roundCounter);
+        counter.textContent = `Round ${roundCounter+1}`;
+    }
+)}
+
 
 /*
 //play 5 rounds
 let playerScore = 0;
 let computerScore = 0;
 for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("rock, paper, scissors, shoot!");
+
     const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
-    function score() {
-        if (result === 'win') {
-            playerScore += 1;
-        }
-        else if (result === 'lose') {
-            computerScore += 1;
-        }
-        else {
-            return; 
-        }
-    }
+    */
+    
+/*
+//declares result in console
     score();
     function declare() {
         if (result === 'tie') {
@@ -65,7 +131,8 @@ for (let i = 0; i < 5; i++) {
             return("something went wrong")
         }
     }
-console.log(declare());
+//console.log(declare());
+
 }
 
 function finalScore() {
@@ -80,5 +147,5 @@ else {
 }
 }
 
-console.log(finalScore());
+//console.log(finalScore());
 */
